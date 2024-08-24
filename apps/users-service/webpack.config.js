b0f096446ee2b2e01cd48502af6c805fbe20fc3a
pavlokobyliatskyi <pavlokobyliatskyi@gmail.com>
2024-08-24 17:51:32 +0300

    refactor(users-service): change the output directory

--- apps/users-service/webpack.config.js
+++ apps/users-service/webpack.config.js
@@ -3,7 +3,7 @@ const { join } = require('path');
 
 module.exports = {
   output: {
-    path: join(__dirname, '../../dist/apps/users'),
+    path: join(__dirname, '../../dist/apps/users-service'),
   },
   plugins: [
     new NxAppWebpackPlugin({
